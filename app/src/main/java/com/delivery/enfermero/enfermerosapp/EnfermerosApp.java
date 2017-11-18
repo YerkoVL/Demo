28dfc19309f135ebf465fad046a0e29469a30099
yerko.a.vera.lezama <1311539_y3rk0>
2017-11-17 20:23:51 -0500

    Initial commit

---
+++ app/src/main/java/com/delivery/enfermero/enfermerosapp/EnfermerosApp.java
@@ -0,0 +1,15 @@
+package com.delivery.enfermero.enfermerosapp;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class EnfermerosApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
